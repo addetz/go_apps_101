e7191f24366ad30471ba43559df5b29f69670b3b
addetz <adelina.simion@deliveroo.co.uk>
2019-09-27 16:12:51 +0100

    Exercise - Print the price of a menu Item

--- models/item.go
+++ models/item.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 // EXERCISE 1: Define Item struct
 // ----------------------------
 type Item struct {
@@ -12,10 +14,7 @@ type Item struct {
 // EXERCISE 3: Print the price of a menu Item
 // ----------------------------------------
 func (i Item) PrintWithPrice() string {
-	//present the item in the format Name: PriceCurrency
-	//NOTE: fmt.Sprintf allows us to format strings just like with fmt.Println
-	//https://golang.org/pkg/fmt/#Sprintf
-	return ""
+    return fmt.Sprintf("%s: %v%s", i.Name, i.Price, i.CurrencyCode)
 }
 
 // EXERCISE 4: Change price of a menu item
