38d4af93b637c340a4f8dc2d212be47a34f6519f
addetz <adelina.simion@deliveroo.co.uk>
2019-09-27 16:19:42 +0100

    Exercise - Change price of a menu item

--- models/item.go
+++ models/item.go
@@ -5,21 +5,21 @@ import "fmt"
 // EXERCISE 1: Define Item struct
 // ----------------------------
 type Item struct {
-    ID string
-    Name string
-    Price float64
-    CurrencyCode string
+	ID           string
+	Name         string
+	Price        float64
+	CurrencyCode string
 }
 
 // EXERCISE 3: Print the price of a menu Item
 // ----------------------------------------
 func (i Item) PrintWithPrice() string {
-    return fmt.Sprintf("%s: %v%s", i.Name, i.Price, i.CurrencyCode)
+	return fmt.Sprintf("%s: %v%s", i.Name, i.Price, i.CurrencyCode)
 }
 
 // EXERCISE 4: Change price of a menu item
 // -------------------------------------
-//func (? ???) ChangePrice(price float64, code string) {
-// Should this method take a pointer receiver?
-// assign new price & currency code here
-//}
+func (i *Item) ChangePrice(p float64, code string) {
+	i.Price = p
+	i.CurrencyCode = code
+}
